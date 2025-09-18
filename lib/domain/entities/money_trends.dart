2a3cbc3d1ed49c2b04a0b1d40bbbae6c72479ada
ayomidepeat <peatayomide@gmail.com>
2025-09-18 11:58:01 +0100

    feat:money trends

---
+++ lib/domain/entities/money_trends.dart
@@ -0,0 +1,15 @@
+class MoneyTrends {
+  final String title;
+  final String image;
+  final String source;
+  final String description;
+  final String url;
+
+  MoneyTrends({
+    required this.title,
+    required this.image,
+    required this.source,
+    required this.description,
+    required this.url,
+  });
+}
